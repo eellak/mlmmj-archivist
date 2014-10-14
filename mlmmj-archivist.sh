d8e5898e731c086e6d601364fd55abe94845d9b3
Manolis Tzanidakis <mtzanidakis@gmail.com>
2014-10-14 14:25:20 +0300

    add public_html in conf. temp path in wip

--- mlmmj-archivist.sh
+++ mlmmj-archivist.sh
@@ -62,7 +62,7 @@ _datefmt() {
 		Dec) _month=12 ;;
 	esac
 
-	# get the year
+	#} get the year
 	_year="$(echo ${_datestr} | awk '{ print $4 }')"
 
 	# 'return' the complete string
@@ -81,7 +81,75 @@ test -s ${_conffile} \
 
 # get a list or mailing lists to process
 # (aka the ones that contain archivist in control dir)
-for _listpath in $(find ${_mlmmj_spool} -type f -name 'archivist'); do
-	# the list path
-	echo $_listpath
+for _listpath in \
+	$(find ${_mlmmj_spool} -type f -name 'archivist' | \
+		sed -e 's:/control/archivist::g')
+do
+	# clear vars to avoid confusion
+	unset _curindex _lastindex
+
+	# check the index file and skip empty lists
+	test -s "${_listpath}/index" \
+		&& _curindex="$(cat ${_listpath}/index)" \
+		|| continue
+
+	# the archivist directory where we store mlmmj-archivist
+	# related files
+	_workpath="${_listpath}/archivist"
+
+	# create the archivist directory if not available
+	test -d "${_workpath}" || install -d -m 0755 "${_workpath}"
+
+	# read the last index
+	test -s "${_workpath}/lastindex" \
+		&& _lastindex="$(cat ${_workpath}/lastindex)"
+
+	# skip lists that are up to date
+	if [ "${_lastindex}" ] && [ ${_curindex} -le ${_lastindex} ]; then
+		continue
+	fi
+
+	# list short name. use cut to get only the listname when
+	# domain/listname structure is used
+	_shortname=$(echo ${_listpath} | sed -e "s:${_mlmmj_spool}/::g" | \
+		cut -d '/' -f 2)
+
+	# create the output directory if not available
+	test -d "${_public_html}" || install -d -m 0755 "${_public_html}"
+
+	# create list output directory if not available
+	_listout="${_public_html}/${_shortname}"
+
+	test -d "${_listout}" || install -d -m 0755 "${_listout}"
+
+	# parse messages to create the list archive
+	for _msg in $(${_seq} ${_lastindex} ${_curindex}); do
+		# the message file
+		_msgfile="${_listpath}/archive/${_msg}"
+
+		# skip unavailable messages
+		test -s "${_msgfile}" || continue
+
+		# get the message date and convert it to the message month
+		_msgdate="$(awk \
+			'/Date: / { print substr($0, index($0, $2))}' \
+			${_msgfile})"
+		_msgmonth=$(_datefmt "${_msgdate}")
+
+		# create month directory if not available
+		test -d "${_listout}/${_msgmonth}" \
+			|| install -d -m 0755 "${_listout}/${_msgmonth}"
+
+		# XXX: replace with actual mhonarc command
+		# XXX: remove last 4 lines - the list signature
+		mhonarc -idxfname index.html \
+			-outdir "${_listout}/${_msgmonth}" \
+			-lang "el" -spammode \
+			-addressmodifycode 's/@/ [ at ] /; s/\./ [ dot ] /g;' \
+			-modifybodyaddresses \
+			-title "${_shortname}" \
+			-add < "${_msgfile}"
+
+		# XXX separate attachments (paths/urls)
+	done
 done
